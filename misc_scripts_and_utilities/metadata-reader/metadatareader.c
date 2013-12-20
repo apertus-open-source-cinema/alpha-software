2831fe2e82fdd3108dd38758f906e89a93a2de88
Sebastian Pichelhofer <sebastian.pichelhofer@gmail.com>
2013-12-20 22:54:42 +0100

    added more registers

--- misc_scripts_and_utilities/metadata-reader/metadatareader.c
+++ misc_scripts_and_utilities/metadata-reader/metadatareader.c
@@ -52,9 +52,9 @@ int main (int argc, char* argv[]) {
 
 
 	// Header
-	printf ("------------------------------------------------------------------------------\n");
-	printf ("Register\tBinary 16bit\t\tHex\tDecimal\tComment\n");
-	printf ("------------------------------------------------------------------------------\n");
+	printf ("-----------------------------------------------------------------------------------\n");
+	printf ("Register\tName\t\t\tDecimal\tValue\tDescription\n");
+	printf ("-----------------------------------------------------------------------------------\n");
 
 	int i;
 	for (i=0; i < 128; i++) {
@@ -82,24 +82,30 @@ int main (int argc, char* argv[]) {
 
 		//Comment
 		if (i == 0)
-			printf("not used");
+			printf("0\t\tnot used");
 		if (i == 1)
 			printf("\n1[15:0]\t\tNumber_lines_tot:\t%d\tnumber of used sensor lines\n", swap_endian(registers[i]));
-		if (i > 1 && i < 34)
-			printf("Y_start");
+		if (i > 1 && i < 10)
+			printf("%u[15:0]\t\t\Y_start:\t\t%d\n",i ,swap_endian(registers[i]));
+		if (i > 9 && i < 34)
+			printf("%u[15:0]\t\Y_start:\t\t%d\n",i ,swap_endian(registers[i]));
 		if (i > 33  && i < 66)
-			printf("Y_size");
+			printf("%u[15:0]\t\Y_size:\t\t\t%d\n",i ,swap_endian(registers[i]));
+		if (i == 66)
+			printf("%u[15:0]\t\Sub_offset:\t\t%d\n",i ,swap_endian(registers[i]));
+		if (i == 67)
+			printf("%u[15:0]\t\Sub_step:\t\t%d\n",i ,swap_endian(registers[i]));
 		if (i == 68) {
-			printf("\n");
+			//printf("\n");
 			if ((get_bit(swap_endian(registers[i]), 3)))
 				printf("68[3]\t\tColor_exp:\t\t1\tmonochrome sensor");
 			else
 				printf("68[3]\t\tColor_exp:\t\t0\tcolor sensor");
 			printf("\n");
 			if ((get_bit(swap_endian(registers[i]), 2)))
-				printf("68[2]\t\tBin_en:\t\t1\tbinning enabled");
+				printf("68[2]\t\tBin_en:\t\t\t1\tbinning enabled");
 			else
-				printf("68[2]\t\tBin_en:\t\t0\tbinning disabled");
+				printf("68[2]\t\tBin_en:\t\t\t0\tbinning disabled");
 			printf("\n");
 			if ((get_bit(swap_endian(registers[i]), 1)))
 				printf("68[1]\t\tSub_en:\t\t\t1\timage subsampling enabled");
@@ -110,6 +116,36 @@ int main (int argc, char* argv[]) {
 				printf("68[0]\t\tColor:\t\t\t1\tmonochrome sensor");
 			else
 				printf("68[0]\t\tColor:\t\t\t0\tcolor sensor");
+		}
+		if (i == 69) {
+			printf("\n%u[1:0]\t\tImage_flipping:\t\t%d\t",i ,get_bits(swap_endian(registers[i]), 0, 2));
+			switch (get_bits(swap_endian(registers[i]), 0, 2)) {
+				case 0:
+					printf("No image flipping");
+					break;
+				case 1:
+					printf("Image flipping in X");
+					break;
+				case 2:
+					printf("Image flipping in Y");
+					break;				
+				case 3:
+					printf("Image flipping in X and Y");
+					break;
+			}
+		}
+		if (i == 70) {
+			printf("\n%u[0]\t\tExp_dual:\t\t%d\t",i ,get_bits(swap_endian(registers[i]), 0, 1));
+			if (get_bits(swap_endian(registers[i]), 0, 1))
+				printf("ON");
+			else
+				printf("OFF");
+			printf("\t\t\tHDR interleaved coloumn mode\n");
+			printf("%u[1]\t\tExp_ext:\t\t%d\t",i ,get_bits(swap_endian(registers[i]), 1, 1));
+			if (get_bits(swap_endian(registers[i]), 1, 1))
+				printf("External Exposure Mode");
+			else
+				printf("Internal Exposure Mode");
 		}
 		if (i == 115) {
 			printf("\n115[3]\t\tPGA_div:\t\t%d\t", get_bits(swap_endian(registers[i]), 3, 1));
@@ -137,7 +173,7 @@ int main (int argc, char* argv[]) {
 		}
 
 		if (i == 116) {
-			printf("\n116[7:0]\tADC_range:\t\t%d\t", get_bits(swap_endian(registers[i]), 0, 8));
+			printf("116[7:0]\tADC_range:\t\t%d\t", get_bits(swap_endian(registers[i]), 0, 8));
 			switch (get_bits(swap_endian(registers[i]), 0, 8)) {
 				case 205:
 					printf("8 bit mode");
@@ -171,6 +207,6 @@ int main (int argc, char* argv[]) {
 		
 		
 	}
-
+	printf("\n");
    	return 0;
 }
